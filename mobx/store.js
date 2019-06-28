b637f8e16de1b07e55fd5343928e58db0770ef8b
VengerA <uyagmur123@gmail.com>
2019-06-28 16:41:26 +0300

    Whole Project

---
+++ mobx/store.js
@@ -0,0 +1,71 @@
+import { observable, action } from "mobx";
+
+
+class MainStore {
+    @observable taskStyle = {
+        // textAlign: 'right',
+        // padding: 5,
+        // backgroundColor: '#f2f6f5',
+        // borderRadius: 10,
+        // width: 200
+    }
+
+    @observable taskList = [
+    ]
+
+    @observable data = {
+        id: null,
+        task: "fuufkkfa", 
+        edit: false, 
+        done: false, 
+        style: null
+    }
+
+    @action takeInput = (data) => {
+        this.taskList.push(data)
+    }   
+
+    @action makeEditable = (id) => {
+        const copiedData = this.taskList.find((copiedData) =>(copiedData.id == id))
+        copiedData.edit = true
+    }
+
+    @action saveInput = (id, text) => {
+        const copiedList = this.taskList.slice()
+        const copiedData = copiedList.find((copiedData) => copiedData.id == id)
+        copiedData.task = text
+        
+    }
+
+    @action completedTask = (id) => {
+        const isDone = this.taskList.find((data) => data.id == id).done
+        if(isDone === false){
+            this.taskList.find((data) => data.id == id).done = true
+          }
+          else {
+            this.taskList.find((data) => data.id == id).done = false
+          }
+    }
+
+    @action doneStyle = (id) => {
+        const node = this.taskList.find((data) => data.id == id)
+        if (node.done === false) {
+            node.style = null
+            this.taskList[node.id] = node
+        }
+        else {
+            const style = {
+                // backgroundColor: '#A7F432',
+                // textAlign: 'right',
+                // padding: 5,
+                // borderRadius: 10,
+                // width: 200
+            }
+            node.style = null
+            this.taskList[node.id] = node
+        }
+    }
+}
+
+const store = new MainStore();
+export default store;
